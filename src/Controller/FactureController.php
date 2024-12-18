cf8590892b8ad10f6c66b1cca9dfd9041bcaaba6
Izukend <98744675+Izukend@users.noreply.github.com>
2024-12-18 14:44:13 +0100

    18/12/2024 Ajout du début du CRUD Facture

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -2,17 +2,73 @@
 
 namespace App\Controller;
 
+use App\Entity\Facture;
+use App\Form\FactureType;
+use App\Repository\FactureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class FactureController extends AbstractController
+#[Route('/facture')]
+final class FactureController extends AbstractController
 {
-    #[Route('/factures', name: 'app_factures')]
-    public function index(): Response
+    #[Route(name: 'app_factures', methods: ['GET'])]
+    public function index(FactureRepository $factureRepository): Response
     {
-        return $this->render('factures/list.html.twig', [
-            'controller_name' => 'FacturesController',
+        return $this->render('facture/index.html.twig', [
+            'factures' => $factureRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_facture_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $facture = new Facture();
+        $form = $this->createForm(FactureType::class, $facture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($facture);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_factures', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('facture/new.html.twig', [
+            'facture' => $facture,
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/{numero}/edit', name: 'app_facture_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Facture $facture, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FactureType::class, $facture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_factures', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('facture/edit.html.twig', [
+            'facture' => $facture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{numero}', name: 'app_facture_delete', methods: ['POST'])]
+    public function delete(Request $request, Facture $facture, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$facture->getNumero(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($facture);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_facture_index', [], Response::HTTP_SEE_OTHER);
     }
 }
