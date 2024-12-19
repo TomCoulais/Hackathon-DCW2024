cc2c005a75e8ec9709750dad269269cb17ebb423
Tom <157611387+TomCoulais@users.noreply.github.com>
2024-12-19 09:26:01 +0100

    Update FactureController.php

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -3,30 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\Facture;
-use App\Form\FactureType;
+use App\Form\FactureAddType;
+use App\Form\FactureEditType;
 use App\Repository\FactureRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/facture')]
-final class FactureController extends AbstractController
+class FactureController extends AbstractController
 {
-    #[Route(name: 'app_factures', methods: ['GET'])]
-    public function index(FactureRepository $factureRepository): Response
+    #[Route('/factures', name: 'app_factures', methods: ['GET'])]
+    public function index(Request $request, FactureRepository $factureRepository): Response
     {
+        $search = $request->query->get('search', '');
+    
+        $facturesQueryBuilder = $factureRepository->createQueryBuilder('f')
+            ->leftJoin('f.client', 'c')
+            ->where('c.name LIKE :search')
+            ->orWhere('f.statut LIKE :search')
+            ->setParameter('search', '%' . $search . '%'); 
+    
+        $factures = $facturesQueryBuilder->getQuery()->getResult();
+    
         return $this->render('facture/index.html.twig', [
-            'factures' => $factureRepository->findAll(),
+            'factures' => $factures,
+            'search' => $search,
         ]);
     }
+    
 
     #[Route('/new', name: 'app_facture_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $facture = new Facture();
-        $form = $this->createForm(FactureType::class, $facture);
+        $form = $this->createForm(FactureAddType::class, $facture);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -38,37 +50,35 @@ final class FactureController extends AbstractController
 
         return $this->render('facture/new.html.twig', [
             'facture' => $facture,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
-
-    #[Route('/{numero}/edit', name: 'app_facture_edit', methods: ['GET', 'POST'])]
+    #[Route('/edit/{id}', name: 'app_facture_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Facture $facture, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(FactureType::class, $facture);
+        $form = $this->createForm(FactureEditType::class, $facture);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-
             return $this->redirectToRoute('app_factures', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('facture/edit.html.twig', [
             'facture' => $facture,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/{numero}', name: 'app_facture_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_facture_delete', methods: ['POST'])]
     public function delete(Request $request, Facture $facture, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$facture->getNumero(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $facture->getId(), $request->request->get('_token'))) {
             $entityManager->remove($facture);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_facture_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_factures', [], Response::HTTP_SEE_OTHER);
     }
 }
